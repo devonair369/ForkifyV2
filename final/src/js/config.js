95cbc10b254f3b5a7410b1ae15d19513ce78fe08
Qreceipt <57878939+wildcard69dev@users.noreply.github.com>
2022-03-06 15:27:26 -0500

    Finish initial commit

---
+++ final/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '60477a0d-dccc-4edf-8b24-74df3880e77e';
+export const MODAL_CLOSE_SEC = 2;
